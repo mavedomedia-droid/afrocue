faf627f0964376d943f1190f4f8f064afb664495
mavedomedia-droid <mavedomedia@gmail.com>
2026-04-05 11:41:09 +0100

    Update afrocue.js

--- afrocue.js
+++ afrocue.js
@@ -4,7 +4,7 @@
    Set SCRIPT_URL below once you deploy the GAS web app.
    ============================================================ */
 
-var SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxK9Rqq_H9U5P1nqvPF48z7x4n7aBZAvYNzHcg77jvRLJ9Ef-_uEkZpk8oFYaDZIJ0mzw/exec';
+var SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyKn1t2c4gSmhNFUz40cImR_BYTxmsn3G_ahZrjRbBgXkg0A1ZULtiLn-DSXPvSLlmH/exec';
 window.AFROCUE_SCRIPT_URL = SCRIPT_URL;
 
 
