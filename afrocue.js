13904ade8baa5b75abb10ce0ed7da8f1f0fe75eb
mavedomedia-droid <mavedomedia@gmail.com>
2026-09-10 15:09:39 +0100

    Update afrocue.js

--- afrocue.js
+++ afrocue.js
@@ -3,7 +3,7 @@
    ============================================================ */
 
 // 👇 Set your Google Apps Script URL once here
-var AFROCUE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyeARt-_vK9lDvdki3hLhQ89wcE1NC3ElHXvdKfbp71w3D0HujWse8DgMe4qnz-aite/exec';
+var AFROCUE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxQCTSjYSo65po4qGrTHl1zN0qR82ySCXiWkCQ_nD52UTB3iugwvwCSMzSjczaQBb_G/exec';
 
 // Expose it for pages that expect different names
 window.AFROCUE_SCRIPT_URL = AFROCUE_SCRIPT_URL;
